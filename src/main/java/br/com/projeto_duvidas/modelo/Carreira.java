8b20abf36307611cc7514754e4ccfed6a86ffb9e
thaisandre <thaisandre.sc@gmail.com>
2017-11-30 21:40:38 -0200

    atualizando a database

---
+++ src/main/java/br/com/projeto_duvidas/modelo/Carreira.java
@@ -0,0 +1,78 @@
+package br.com.projeto_duvidas.modelo;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Carreira {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	private String linguagem;
+	
+	private String link;
+	
+	@Column(columnDefinition = "LONGTEXT")
+	private String descricao;
+	
+	@OneToMany
+	private List<Curso> cursos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLinguagem() {
+		return linguagem;
+	}
+
+	public void setLinguagem(String linguagem) {
+		this.linguagem = linguagem;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Curso> getCursos() {
+		return cursos;
+	}
+
+	public void setCursos(List<Curso> cursos) {
+		this.cursos = cursos;
+	}
+}
